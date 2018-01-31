62c8a3efab94f716bc7752f0ada874ad11e34671
htfei <why2fly@aliyun.com>
2018-01-31 18:11:46 +0800

    2 get !

--- baiduwenku.py
+++ baiduwenku.py
@@ -1,50 +1,47 @@
 # -*- coding:UTF-8 -*-
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import re
+'''
+aaa
+'''
 import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 if __name__ == '__main__':
+    OPT = webdriver.ChromeOptions()
+    OPT.add_argument('user-agent="Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) \
+    AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19"')
+    DRIVER = webdriver.Chrome(chrome_options=OPT)  # 1111
+    DRIVER.get('https://wenku.baidu.com/view/aa31a84bcf84b9d528ea7a2c.html')
 
-	options = webdriver.ChromeOptions()
-	options.add_argument('user-agent="Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19"')
-	driver = webdriver.Chrome('J:\迅雷下载\chromedriver.exe', chrome_options=options)
-	driver.get('https://wenku.baidu.com/view/aa31a84bcf84b9d528ea7a2c.html')
+    MYHTML = DRIVER.page_source
+    BF1 = BeautifulSoup(MYHTML, 'lxml')
+    TITLE = BF1.select("div.doc-title")[0].string
+    print('文章标题：%s' % TITLE)
+    JIA = 1
 
-	html = driver.page_source
-	bf1 = BeautifulSoup(html, 'lxml')
-	result = bf1.find_all(class_='rtcspage')
-	bf2 = BeautifulSoup(str(result[0]), 'lxml')
-	title = bf2.div.div.h1.string
-	pagenum = bf2.find_all(class_='size')
-	pagenum = BeautifulSoup(str(pagenum), 'lxml').span.string
-	pagepattern = re.compile('页数：(\d+)页')
-	num = int(pagepattern.findall(pagenum)[0])
-	print('文章标题：%s' % title)
-	print('文章页数：%d' % num)
+    while True:
+        TEXTS = DRIVER.find_elements_by_css_selector(
+            "div.content.singlePage div p")
+        for each_text in TEXTS:
+            inner_text = DRIVER.execute_script(
+                "return arguments[0].innerText;", each_text)
+            print(inner_text + '\n')
+            #break
 
-
-	while True:
-		num = num / 5.0
-		html = driver.page_source
-		bf1 = BeautifulSoup(html, 'lxml')
-		result = bf1.find_all(class_='rtcspage')
-		for each_result in result:
-			bf2 = BeautifulSoup(str(each_result), 'lxml')
-			texts = bf2.find_all('p')
-			for each_text in texts:
-				main_body = BeautifulSoup(str(each_text), 'lxml')
-				for each in main_body.find_all(True):
-					if each.name == 'span':
-						print(each.string.replace('\xa0',''),end='')
-					elif each.name == 'br':
-						print('')
-			print('\n')
-		if num > 1:
-			page = driver.find_elements_by_xpath("//div[@class='page']")
-			driver.execute_script('arguments[0].scrollIntoView();', page[-1]) #拖动到可见的元素去
-			nextpage = driver.find_element_by_xpath("//a[@data-fun='next']")
-			nextpage.click()
-			time.sleep(3)
-		else:
-			break
+        #只有第一次需要“加载更多”
+        if JIA <= 1:
+            flod_button = DRIVER.find_element_by_xpath(
+                "//span[@class='fold-arrow-lower']")  # TODO 找不到会报错，尝试解决
+            flod_button.click()
+            JIA = 2
+        #下一页
+        nextpage = DRIVER.find_element_by_xpath(
+            "//div[@class='x-page next']")   # TODO 找不到会报错，尝试解决
+        if nextpage:
+            print('==========next page==========\n')
+            DRIVER.execute_script(
+                'arguments[0].scrollIntoView(false);', nextpage)  # 拖动到可见的元素去
+            nextpage.click()
+            time.sleep(3)
+        else:
+            break
--- shuaia.py
+++ shuaia.py
@@ -6,44 +6,45 @@ import os
 import time
 
 if __name__ == '__main__':
-	list_url = []
-	for num in range(1,3):
-		if num == 1:
-			url = 'http://www.shuaia.net/index.html'
-		else:
-			url = 'http://www.shuaia.net/index_%d.html' % num
-		headers = {
-				"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-		}
-		req = requests.get(url = url,headers = headers)
-		req.encoding = 'utf-8'
-		html = req.text
-		bf = BeautifulSoup(html, 'lxml')
-		targets_url = bf.find_all(class_='item-img')
-		
-		for each in targets_url:
-			list_url.append(each.img.get('alt') + '=' + each.get('href'))
+    list_url = []
+    for num in range(1, 3):
+        if num == 1:
+            url = 'http://www.shuaia.net/index.html'
+        else:
+            url = 'http://www.shuaia.net/index_%d.html' % num # 字符串处理
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+        }
+        req = requests.get(url=url, headers=headers)
+        req.encoding = 'utf-8'
+        html = req.text
+        bf = BeautifulSoup(html, 'lxml')
+        targets_url = bf.find_all(class_='item-img')
 
-	print('连接采集完成')
+        for each in targets_url:
+            list_url.append(each.img.get('alt') + '=' + each.get('href')) # 111111111 只用append一次即可
 
-	for each_img in list_url:
-		img_info = each_img.split('=')
-		target_url = img_info[1]
-		filename = img_info[0] + '.jpg'
-		print('下载：' + filename)
-		headers = {
-			"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-		}
-		img_req = requests.get(url = target_url,headers = headers)
-		img_req.encoding = 'utf-8'
-		img_html = img_req.text
-		img_bf_1 = BeautifulSoup(img_html, 'lxml')
-		img_url = img_bf_1.find_all('div', class_='wr-single-content-list')
-		img_bf_2 = BeautifulSoup(str(img_url), 'lxml')
-		img_url = 'http://www.shuaia.net' + img_bf_2.div.img.get('src')
-		if 'images' not in os.listdir():
-			os.makedirs('images')
-		urlretrieve(url = img_url,filename = 'images/' + filename)
-		time.sleep(1)
+    print('连接采集完成')
 
-	print('下载完成！')
+    for each_img in list_url:
+        img_info = each_img.split('=') # 111111111
+        target_url = img_info[1]
+        filename = img_info[0] + '.jpg'  
+        print('下载：' + filename)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+        }
+        img_req = requests.get(url=target_url, headers=headers)
+        img_req.encoding = 'utf-8'
+        img_html = img_req.text
+        img_bf_1 = BeautifulSoup(img_html, 'lxml')
+        img_url = img_bf_1.find_all('div', class_='wr-single-content-list')
+        img_bf_2 = BeautifulSoup(str(img_url), 'lxml')
+        img_url = 'http://www.shuaia.net' + img_bf_2.div.img.get('src')
+        if 'images' not in os.listdir():  # os.listdir()无参代表当前目录 cmd中的当前路径
+            os.makedirs('images')
+        # urlretrieve()方法直接将远程数据下载到本地
+        urlretrieve(url=img_url, filename='images/' + filename)
+        time.sleep(1)
+
+    print('下载完成！')
